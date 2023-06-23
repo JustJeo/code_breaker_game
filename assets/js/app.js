9c135531fb45e4e0e97a54e8fa120dfde98aaf3b
Jeo <j.cabrigas.writer@gmail.com>
2023-06-22 23:41:39 -0700

    Gets fx to work after adding 'defer' to js so that script runs after content

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,17 @@
 console.log("AYO! This is Jeo!");
 
-// Listens for click on "Secret Code" btn
-let random_btn = document.getElementById("random-code-btn");
-random_btn.addEventListener("click", getSecretCode);
-
 // Get Random Number for Secret Code
 function getRandomNumber() {
-    return Math.floor(Math.random()+10);
+    return Math.floor(Math.random()*10);
 }
 
 function getSecretCode() {
-    console.log("This works!");
-    // document.getElementById("code1").innerHTML = getRandomNumber();
-    // document.getElementById("code2").innerHTML = getRandomNumber();
-    // document.getElementById("code3").innerHTML = getRandomNumber();
-    // document.getElementById("code4").innerHTML = getRandomNumber();
+    document.getElementById("code1").innerHTML = getRandomNumber();
+    document.getElementById("code2").innerHTML = getRandomNumber();
+    document.getElementById("code3").innerHTML = getRandomNumber();
+    document.getElementById("code4").innerHTML = getRandomNumber();
 }
+
+// Listens for click on "Secret Code" btn
+let random_btn = document.getElementById("random-code-btn");
+random_btn.addEventListener("click", getSecretCode);
