bc5a37f1f7587d7530f6bbb7f6dacaef02e3f7cd
Jeo <j.cabrigas.writer@gmail.com>
2023-06-23 00:00:03 -0700

    Adds player guess fx

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,22 @@
 console.log("AYO! This is Jeo!");
 
-// Get Random Number for Secret Code
+// ~~~~~~~~~~ Initial Load Declarations ~~~~~~~~~~
+// Player Guess
+let player_guess = 10;
+
+// Start Btn
+let start_btn = document.getElementById("random-code-btn");
+
+
+// Listens for click on "Secret Code" btn
+start_btn.addEventListener("click", getSecretCode);
+
+// Randomly grabs one number
 function getRandomNumber() {
     return Math.floor(Math.random()*10);
 }
 
+// Uses random number fx to generate secret code for game.
 function getSecretCode() {
     document.getElementById("code1").innerHTML = getRandomNumber();
     document.getElementById("code2").innerHTML = getRandomNumber();
@@ -12,6 +24,22 @@ function getSecretCode() {
     document.getElementById("code4").innerHTML = getRandomNumber();
 }
 
-// Listens for click on "Secret Code" btn
-let random_btn = document.getElementById("random-code-btn");
-random_btn.addEventListener("click", getSecretCode);
+// Place secret code into secret array
+
+// Place player code into player array
+
+// Check player code to secret code
+
+// If player code == secret code, then player wins
+
+// if player code != secret code, then -1 player guess and player gets to guess again
+
+// if player guess == 0, then game is over
+function getGuessAmount() {
+    if(player_guess == 0) {
+        return "Game Over";
+    } else {
+        player_guess --;
+        return player_guess + "guesses left";
+    }
+}
